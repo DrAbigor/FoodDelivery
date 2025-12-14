210cb9681b02263bc30009d54f11e5247b459d80
Eddri <Eddri@LAPTOP-HLNRCKDD>
2025-12-14 18:13:21 +0800

    Added Login page and Register pages with without database

--- Food Delivery/Program.cs
+++ Food Delivery/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped<Food_Delivery.Services.AuthState>();
+
 
 var app = builder.Build();
 
---
+++ Food Delivery/Service/AuthState.cs
@@ -0,0 +1,17 @@
+﻿namespace Food_Delivery.Services
+{
+    public class AuthState
+    {
+        public bool IsLoggedIn { get; private set; } = false;
+
+        public void Login()
+        {
+            IsLoggedIn = true;
+        }
+
+        public void Logout()
+        {
+            IsLoggedIn = false;
+        }
+    }
+}
